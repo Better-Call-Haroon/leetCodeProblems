dac946d8c88892d84723d976e5e782d7e4ad02d5
Better-Call-Haroon <113617367+MalikHaroonKhokhar@users.noreply.github.com>
2023-11-27 23:36:00 +0500

    google leetcode

---
+++ ParenthesisStack.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        if s[0]==']' or s[0]==')' or s[0]=='}':
+            return False 
+        else:
+            for i in s:
+                if i == '(' or i == '{' or i == '[':
+                    stack.append(i)
+                elif i==')' and stack and stack[-1]=='(':
+                    stack.pop()
+                elif i=='}' and stack and stack[-1]=='{':
+                    stack.pop()
+                elif i==']' and stack and stack[-1]=='[':
+                    stack.pop()
+                else: 
+                    return False
+        return not stack 
