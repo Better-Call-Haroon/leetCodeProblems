52091d21674855a30ed8a7b7dfaeff877993873e
Malik Haroon Khokhar <malikharoonkhokhar789@gmail.com>
2024-08-14 12:49:47 +0500

    Update Multiplesof3or5.cpp

--- Multiplesof3or5.cpp
+++ Multiplesof3or5.cpp
@@ -1,23 +1,12 @@
 #include <iostream>
-
-
 int main()
-
 {
-    int arr[998];
-    int start = 1;
     int counter = 0;
-    for (int i = 0; i <= 998; i++) {
-        arr[i] = start;
-        start++;
-    }
-    for (int i = 0; i <= 998; i++) {
-        if (arr[i] % 3==0 || arr[i] % 5 == 0) {
-            counter = counter + arr[i];
-        }
-        
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3==0 || i % 5 == 0) {
+            counter = counter + i;
+        }        
     }
     std::cout << counter << std::endl;
-
     return 0;
 }
